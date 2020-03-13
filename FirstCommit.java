40295092dd76ef97bbf4479d3efe57fb5052f8c6
StevenP635 <61333994+StevenP635@users.noreply.github.com>
2020-03-13 08:26:50 -0500

    Update FirstCommit.java

--- FirstCommit.java
+++ FirstCommit.java
@@ -32,5 +32,6 @@ public class FirstCommit {
 		System.out.println( "My name is Abdelrhman Alaroudi and my hobby is playing soccer" );
 		System.out.println( "My name is Kassie Garcia and my hobby is tarot reading.");
 		System.out.println( "My name is Heena Rathore and my hobby is traveling");
+		System.out.println( "My name is Steven Paredes and my hobby is cooking." );
 	}
 }
