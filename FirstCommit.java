9226309ec3a1c011e39ab4e29c0bd144d74192cc
Elijah Lozano <elijahlozano@icloud.com>
2020-03-15 13:34:05 -0500

    Added my name to the file

--- FirstCommit.java
+++ FirstCommit.java
@@ -39,5 +39,6 @@ public class FirstCommit {
 		System.out.println( "My name is Eloy Rodriguez and my hobby is Exotic game hunting" );
 		System.out.println("My name is Dale Graham and my hobby is playing video games");
 		System.out.println("My name is Maria Jose Healey and my hobby is playing basketball");
+		System.out.println("My name is Elijah Lozano and my hobby is watching critically acclaimed movies.");
 	}
 }
