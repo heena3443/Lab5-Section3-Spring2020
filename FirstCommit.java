7b7a2af1cb01465ae32fea9325d30163a4672676
Elijah_Lozano <42485857+eteaclesmyles@users.noreply.github.com>
2020-03-15 20:01:04 -0500

    Added another hobby for good measure

--- FirstCommit.java
+++ FirstCommit.java
@@ -39,6 +39,6 @@ public class FirstCommit {
 		System.out.println( "My name is Eloy Rodriguez and my hobby is Exotic game hunting" );
 		System.out.println("My name is Dale Graham and my hobby is playing video games");
 		System.out.println("My name is Maria Jose Healey and my hobby is playing basketball");
-		System.out.println("My name is Elijah Lozano and my hobby is cycling.");
+		System.out.println("My name is Elijah Lozano and my hobby is cycling and I like video games.");
 	}
 }
